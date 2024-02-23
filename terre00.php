12fedd35124dd7a4eeb44984cfab1e185ff56339
Bakary Dembele <bakgsxr.dembele@gmail.com>
2024-02-23 22:21:21 +0100

    terre00 ok

---
+++ terre00.php
@@ -0,0 +1,9 @@
+<?php
+
+$alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+for ($i=0; $i <= 25 ; $i++) { 
+	echo $alphabet[$i];
+}
+
+echo "\n";
