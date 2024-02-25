f74d1f644d04d2df963c70a3da5957bfd1570015
Bakary Dembele <bakgsxr.dembele@gmail.com>
2024-02-25 16:36:30 +0100

    Début d'inclusion des tests sur terre03

--- terre03.php
+++ terre03.php
@@ -10,7 +10,6 @@
 
 $alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-
 // $alphabet = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122];  // De "a" à "z" en code ASCII. 
 
 $lettre_saisie = $argv[1];  //  La lettre tapée en argument du script.
@@ -20,49 +19,51 @@ $lettre_saisie = $argv[1];  //  La lettre tapée en argument du script.
 
 
 
-
-	/*** S'assurer que l'argument passé au script soit une lettre unique en minuscule ***/
-
-if ($argc > 2) {
-	echo "Une seule lettre s'il vous plaît. \n";
-}
-
-
-
-
-
-
-
-
-	/*** Comparer cet argument avec les éléments du tableau $alphabet pour trouver son index ***/
+/****** Comparer cet argument avec les éléments du tableau $alphabet pour trouver son index ******/
 
 //---------------------------------------------//
 function findKey(){
+	global $lettre_saisie;
 	global $alphabet;
 	foreach ($alphabet as $key => $lettre) {
-		global $lettre_saisie;
 		if ($lettre_saisie === $lettre) {
 			return $key;
 		}
 	}
 }
-//---------------------------------------------//
-// echo findKey();
 
-// echo "\n";
-//---------------------------------------------//
-
-
-
-
-	/*** Afficher à partir de $key ***/
+/****** Afficher à partir de $key ******/
 
 //---------------------------------------------//
-for ($i = findKey(); $i <= 25; $i++) { 
+function printAlphabetSince(){
+	global $alphabet;
+	for ($i = findKey(); $i <= 25; $i++) { 
 	echo $alphabet[$i];
+	}	
+}	
+
+/****** S'assurer que l'argument passé au script soit une lettre unique en minuscule ******/
+
+//----------------------------------------------//
+if ($argc > 2){
+	echo "Une seule lettre s'il vous plaît. \n";
 }
 
-echo "\n";
+if (strlen($lettre_saisie) != 1){
+	echo "Un seul caractère s'il vous plaît. \n";
+	}
+
+if ($argc == 2){
+	printAlphabetSince();
+	echo "\n";
+}
+
+
+
+
+
+
+
 
 //----------------------------------------------//
 
