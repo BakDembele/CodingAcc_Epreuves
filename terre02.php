b61f1d232945ec966a840bf7e74de302b5114ac9
Bakary Dembele <bakgsxr.dembele@gmail.com>
2024-02-27 21:25:46 +0100

    Petit souci détecté dans terre02 -->Ne prend pas en argument les points d'interrogation

--- terre02.php
+++ terre02.php
@@ -5,3 +5,9 @@
 for ($i=1; $i < $argc ; $i++) { 
 	echo $argv[$i] . "\n";
 }
+
+// -------- Attention ! -------- //
+
+/*    Ne prend pas en argument les points d'interrogation    */
+
+// !!!!! En cours d'amélioration !!!!! //
