be1a78e8ba0759b6788dc828d9ecdcbd1da9f50e
Bakary Dembele <bakgsxr.dembele@gmail.com>
2024-03-04 00:55:10 +0100

    terre06 : transformation string en tableau OK bis.

--- terre06.php
+++ terre06.php
@@ -20,7 +20,15 @@ echo "\n";
 var_dump($tableau_des_caracteres);
 echo "\n";
 
+
 $tableau_des_caracteres = str_split($string_saisie);
 
+
+
 var_dump($tableau_des_caracteres);
+echo "\n";
+
+
+
+echo count($tableau_des_caracteres);
 echo "\n";
