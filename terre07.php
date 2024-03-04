6454fd1d5984bd830aca69f49915eb824a200a75
Bakary Dembele <bakgsxr.dembele@gmail.com>
2024-03-04 21:49:30 +0100

    terre07 en cours, manque gestion des caractères autorisés.

---
+++ terre07.php
@@ -0,0 +1,28 @@
+<?php
+
+/************ Taille d'une chaîne ************/
+
+
+$string_saisie = 0;
+$nombre_de_caracteres = 0;
+$tableau_des_caracteres = [];
+
+if ($argc != 2) {
+	exit("Nombre d'argument incorrect !!!\n");
+}
+
+
+$string_saisie = $argv[1];
+
+if (!(ctype_alpha($string_saisie))) {
+	exit("Caractères invalides !!!\n");
+}
+
+$tableau_des_caracteres = str_split($string_saisie);
+
+foreach ($tableau_des_caracteres as $key => $value) {
+	$nombre_de_caracteres += 1;
+}
+
+echo $nombre_de_caracteres;
+echo "\n";
