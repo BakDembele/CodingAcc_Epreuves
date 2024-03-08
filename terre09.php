90234ab191a6243e741769cc1e385d77c44c5d3d
Bakary Dembele <bakgsxr.dembele@gmail.com>
2024-03-08 21:21:45 +0100

    terre09 --> algo OK avec uniquement des carrés parfaits.

--- terre09.php
+++ terre09.php
@@ -3,3 +3,18 @@
 /************ Racine carrée d’un nombre ************/
 
 $nombre_saisi = 0;
+
+function squareRoot(){
+	$nbr = 0;
+	$resultat = 0;
+	global $nombre_saisi;
+	while ($resultat != $nombre_saisi) {
+		$nbr++;
+		$resultat = $nbr * $nbr;
+	}
+		echo $nbr . "\n";
+}
+
+$nombre_saisi = $argv[1];
+
+squareRoot();
